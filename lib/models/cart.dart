fb41e2e863fb07d059fe60ad1502b628841d290c
Michael Propp <michaelpropp8664@gmail.com>
2020-05-18 13:30:05 -0400

    Remove Item from Cart

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -41,6 +41,11 @@ class Cart with ChangeNotifier {
     }
     notifyListeners();
   }
+
+  void removeItem(String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
 }
 
 class CartItem {
--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../models/cart.dart';
+import '../widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -46,7 +47,21 @@ class CartScreen extends StatelessWidget {
                   ],
                 ),
               ),
-            )
+            ),
+            SizedBox(height: 10),
+            Expanded(
+                child: ListView.builder(
+              itemBuilder: (ctx, index) {
+                return CartItemWidget(
+                  id: cart.items.values.toList()[index].id,
+                  productId: cart.items.keys.toList()[index],
+                  price: cart.items.values.toList()[index].price,
+                  quantity: cart.items.values.toList()[index].quantity,
+                  title: cart.items.values.toList()[index].title,
+                );
+              },
+              itemCount: cart.itemCount,
+            )),
           ],
         ));
   }
