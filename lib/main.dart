b932753e4f9f9a350cb96b9d1cab3a643a79a3bd
Michael Propp <michaelpropp8664@gmail.com>
2020-05-20 13:36:54 -0400

    Your Products Page

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import './models/cart.dart';
 import './screens/cart_screen.dart';
 import './models/orders.dart';
 import './screens/orders_screen.dart';
+import './screens/user_products_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -46,6 +47,7 @@ class MyApp extends StatelessWidget {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx) => OrdersScreen(),
+          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
         },
       ),
     );
